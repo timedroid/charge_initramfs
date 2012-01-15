96d539ca494a54540b275e7a181f829b6ad13fee
Matthew Grochowalski <matt@gwalski.com>
2012-01-15 17:13:33 -0500

    Fix dumping bml12 partition

--- sbin/dump_modem.sh
+++ sbin/dump_modem.sh
@@ -1,7 +1,11 @@
 #!/sbin/sh
 
+mount -o remount,rw /
+
 exec >> /tmp/bml_over_mtd.log 2>&1
 
 rm /data/modem.bin
-/sbin/bml_over_mtd dump radio 1954 reservoir 2004 /data/modem/bin
+/sbin/bml_over_mtd dump radio 1954 reservoir 2004 /data/modem.bin
 ln -s /data/modem.bin /dev/block/bml12
+
+mount -o remount,ro /
