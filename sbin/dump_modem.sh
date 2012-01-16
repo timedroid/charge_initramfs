8251e6204e2181f6c14509db3084ddcdb36172f6
Matthew Grochowalski <matt@gwalski.com>
2012-01-15 20:56:53 -0500

    Dump efs partitions and symlink to /dev/nvs
    This makes the radio actually work. :)

--- sbin/dump_modem.sh
+++ sbin/dump_modem.sh
@@ -8,4 +8,10 @@ rm /data/modem.bin
 /sbin/bml_over_mtd dump radio 1954 reservoir 2004 /data/modem.bin
 ln -s /data/modem.bin /dev/block/bml12
 
+if [ ! -e /data/efs.bin ]
+then
+	/sbin/bml_over_mtd dump efs 2002 reservoir 2004 /data/efs.bin
+fi
+ln -s /data/efs.bin /dev/nvs
+
 mount -o remount,ro /
